0046d8199657bbaa81d6057708d2efd055ddb0f6
marsigergo4-del <marsigergo4@gmail.com>
2025-12-17 15:29:17 +0100

    Create Player.java

---
+++ domain/Player.java
@@ -0,0 +1,19 @@
+package org.example.domain;
+
+public class Player {
+    protected String name;
+    protected char mark;
+
+    public Player(String name, char mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getMark() {
+        return mark;
+    }
+}
